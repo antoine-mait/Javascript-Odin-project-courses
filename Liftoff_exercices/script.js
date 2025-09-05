d064559afc20f3e8375c8650f4d0227c61a40d9c
antoine-mait <antoine.maitrias@gmail.com>
2025-09-05 09:22:29 +0200

    liftoff exercices , for/while loop course

---
+++ Liftoff_exercices/script.js
@@ -0,0 +1,14 @@
+const output = document.querySelector(".output");
+output.textContent = "";
+
+for (let i = 10; i >= 0; i--) {
+  const para = document.createElement("p");
+  if (i === 10) {
+    para.textContent = "Countdown 10";
+  } else if (i === 0) {
+    para.textContent = "Blast off";
+  } else {
+    para.textContent = i;
+  }
+  output.appendChild(para);
+}
