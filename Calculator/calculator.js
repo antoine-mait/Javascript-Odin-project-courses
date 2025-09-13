dbb9dfa86cf7bfbe238990d2f2b0b6e87be0a196
antoine-mait <antoine.maitrias@gmail.com>
2025-09-13 18:03:05 +0200

    Basic calcul working, need change to not reset the result on = , to keep going with the calcul

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -6,19 +6,67 @@ const subtract = function(a,b) {
  return a - b ;	
 };
 
-const sum = function(array) {
-  return array.reduce((total, num) => total + num, 0);
-};
-
-const multiply = function(array) {
-  return array.reduce((product, num) => product * num, 1);
+const multiply = function (array) {
+  return array.reduce((product, current) => product * current)
 };
 
 const divide = function(a,b) {
     return a / b ;
 }
 
-const operate = function(operator , a , b){
-    
-}
+const result = document.querySelector("#result");
+const click=[];
+
+document.querySelectorAll("button").forEach((button) => {
+  button.addEventListener("click", btnAction);
+});
+
+function btnAction(event) {
+  const btn = event.target;
+  console.info(btn.innerText);
+  click.push(btn.innerText);
+  
+};
+
+document.querySelector("#btn_equal").addEventListener("click", () => {
+  operate();
+});
+
+function operate(){
+  result.innerHTML = "";
+  let numberA = "";
+  let numberB = "";
+  let operatorSelected = null;
+  let operatorFound = false;
+
+  click.forEach(item =>{
+    if (item === "=") return;
+    if("+-*/".includes(item)){
+      operatorSelected= item;
+      operatorFound= true;
+    }else if (!operatorFound){
+      numberA += item;
+    }else{
+      numberB += item;
+    }
+  })
+
+  numberA = Number(numberA);
+  numberB = Number(numberB);
+
+  if(operatorSelected === "+"){
+    result.textContent = add(numberA , numberB);
+  }
+  else if (operatorSelected ==="-"){
+    result.textContent = subtract(numberA , numberB);
+  }
+  else if (operatorSelected ==="*"){
+    const arrayMultiply = [numberA, numberB]
+    result.textContent = multiply(arrayMultiply);
+  }
+  else if (operatorSelected ==="/"){
+    result.textContent = divide(numberA , numberB);
+  }console.log(result.innerHTML);
+  click.length = 0 ;
+};
 
