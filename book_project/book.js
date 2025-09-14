2c39d432d385b1243630f44e1e14c180a469fb16
antoine-mait <antoine.maitrias@gmail.com>
2025-09-14 17:02:21 +0200

    Book / library exercice

---
+++ book_project/book.js
@@ -0,0 +1,22 @@
+/*
+all book are {} and store in array [];
+need constructor for book
+all book as unique id using crypto.randomUUID()
+function taking arguments to create the book from
+
+function loop through the array , display each book on the page , in a card for each
+
+button to add new book , input user for author , title , numbers of page , read or not .
+in a each book card , add a remove book button and a read button 
+connect the read button with a prototype function that toggle a book instance read status
+*/
+
+const myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // take params, create a book then store it in the array
+}
