20450968c819aa34c08f043acdb55d4422abc2b7
antoine-mait <antoine.maitrias@gmail.com>
2025-11-07 14:45:00 +0100

    ship player and gameboard class done and tested, next step is html css and DOM interaction with it to create the actual game

--- BattleShip/battleship.js
+++ BattleShip/battleship.js
@@ -1,5 +1,3 @@
-const { all } = require("micromatch");
-
 function generateId() {
     return crypto.randomUUID();
 }
@@ -26,41 +24,40 @@ class Ship {
         if (position.length === 1) {
             return [position[0]]
             // horizontal
-        } else if (position[0][0] === position[1][0] && position[0][1] < position[1][1] | position[0][1] > position[1][1]) {
-            const startionPos = position[0]; //[0,0]
+        } else if (position[0][0] === position[1][0] && position[0][1] < position[1][1] || position[0][1] > position[1][1]) {
+            const startingPos = position[0]; //[0,0]
             const endingPos = position[1];//[0,4]
-            let allPos = [startionPos];
+            let allPos = [startingPos];
 
             //  [0] < [4]
-            let digitHorizontal = startionPos[1]
+            let digitHorizontal = startingPos[1]
             while (digitHorizontal < endingPos[1]) {
                 digitHorizontal++;
-                allPos.push([startionPos[0], digitHorizontal])
+                allPos.push([startingPos[0], digitHorizontal])
             }
             while (digitHorizontal > endingPos[1]) {
                 digitHorizontal--;
-                allPos.push([startionPos[0], digitHorizontal])
+                allPos.push([startingPos[0], digitHorizontal])
             }
 
             return allPos
 
             // vertical
-        } else if (position[0][1] === position[1][1] && position[0][0] < position[1][0] | position[0][0] > position[1][0]) {
-            const startionPos = position[0]; //[0,0]
+        } else if (position[0][1] === position[1][1] && position[0][0] < position[1][0] || position[0][0] > position[1][0]) {
+            const startingPos = position[0]; //[0,0]
             const endingPos = position[1];//[4,0]
-            let allPos = [startionPos];
+            let allPos = [startingPos];
 
             //  [0] < [4]
-            let digitHorizontal = startionPos[0]
+            let digitHorizontal = startingPos[0]
             while (digitHorizontal < endingPos[0]) {
                 digitHorizontal++;
-                allPos.push([digitHorizontal, startionPos[1]])
+                allPos.push([digitHorizontal, startingPos[1]])
             }//  [4] > [0]
             while (digitHorizontal > endingPos[0]) {
                 digitHorizontal--;
-                allPos.push([digitHorizontal, startionPos[1]])
+                allPos.push([digitHorizontal, startingPos[1]])
             }
-
             return allPos
         }
     }
@@ -72,14 +69,26 @@ class GameBoard {
         this.size = [[minSize, minSize], [maxSize, maxSize]]
         this.missedShot = [];
         this.allSinked = false;
-        this.ships = []
+        this.ships = [];
     }
     placeShip(length, position) {
+        // Check if ship fits on board
+        // Check if position overlaps with existing ships
         const newShip = new Ship(length, position)
         this.ships.push(newShip)
         return newShip
     }
     receiveAttack(position) {
+
+        const alreadyHit = this.ships.some(ship =>
+            ship.hitCell.some(cell => cell[0] === position[0] && cell[1] === position[1])
+        );
+        const alreadyMissed = this.missedShot.some(cell =>
+            cell[0] === position[0] && cell[1] === position[1]
+        );
+
+        if (alreadyHit || alreadyMissed) return;
+
         for (let ship of this.ships) {
             const hit = ship.occupiedCells.find(cell => cell[0] === position[0] && cell[1] === position[1]);
             if (hit) {
@@ -95,25 +104,47 @@ class GameBoard {
 }
 
 class Player {
-    constructor(GameBoard , userName , playerType = real){
-        this.name = userName || computer;
-        this.playerType = playerType || computer;
-        this.board = GameBoard;
+    constructor(userName, isReal = false) {
+        this.name = userName || "computer";
+        this.isReal = isReal;
+        this.board = new GameBoard(0, 9);
+        this.availableCell = [];
+    }
+    attack(oppenantBoard, position = null) {
+        const positions = oppenantBoard.size
+        const startingPos = positions[0]
+        const endingPos = positions[1]
+        const allCell = this.availableCell
+        let currentRow = startingPos[0];
+        let currentCol = startingPos[1];
+
+        if (allCell.length <= 0) { // generate if allCell is empty
+            // Get all possible Cell
+            while (currentRow <= endingPos[0]) {
+                while (currentCol <= endingPos[1]) {
+                    allCell.push([currentRow, currentCol])
+                    currentCol++;
+                }
+                currentRow++;
+                currentCol = startingPos[1];
+            }
+        }
+
+        if (!this.isReal) { // if its a computer 
+            const randomIndex = Math.floor(Math.random() * allCell.length);
+            const randomCell = allCell[randomIndex];
+            if (randomIndex > -1) { // Select random cell , and remove from allCell array
+                allCell.splice(randomIndex, 1);
+            }
+            oppenantBoard.receiveAttack(randomCell)
+        } else {
+            oppenantBoard.receiveAttack(position)
+        }
     }
 }
 
 module.exports = {
     Ship,
     GameBoard,
+    Player
 }
-
-const myBoard = new GameBoard(0, 9);
-myBoard.placeShip(3, [[0, 0], [0, 2]]);
-myBoard.placeShip(5, [[2, 0], [2, 4]]);
-myBoard.receiveAttack([0, 0]);
-myBoard.receiveAttack([0, 1]);
-myBoard.receiveAttack([0, 2]);
-if (myBoard.allSinked === true) {
-    console.log("Game over!");
-}
-console.log("Total ships:", myBoard.ships.length);
--- BattleShip/battleship.test.js
+++ BattleShip/battleship.test.js
@@ -1,68 +1,67 @@
-const { Ship, GameBoard } = require('./battleship');
+const { Ship, GameBoard, Player } = require('./battleship');
 
-// describe('Ship Class', () => {
-//   test('should create a Ship with lenght and position', () => {
-//     const ship = new Ship('5', '[[0,0],[0,4]]');
+describe('Ship Class', () => {
+  test('should create a Ship with lenght and position', () => {
+    const ship = new Ship('5', '[[0,0],[0,4]]');
 
-//     expect(ship.length).toBe('5');
-//     expect(ship.position).toBe('[[0,0],[0,4]]');
-//     expect(ship.hitCell).toStrictEqual([]);
-//     expect(ship.sunked).toBe(false);
-//   });
+    expect(ship.length).toBe('5');
+    expect(ship.position).toBe('[[0,0],[0,4]]');
+    expect(ship.hitCell).toStrictEqual([]);
+    expect(ship.sunked).toBe(false);
+  });
 
-//   test('should say Hit ship at selectedCell position', () => {
-//     const ship = new Ship(5, [[0, 0], [0, 4]]);
-//     const selectedCell = [0, 3]
+  test('should say Hit ship at selectedCell position', () => {
+    const ship = new Ship(5, [[0, 0], [0, 4]]);
+    const selectedCell = [0, 3]
 
-//     ship.hit(selectedCell);
-//     expect(ship.hitCell).toStrictEqual([[0, 3]]);
+    ship.hit(selectedCell);
+    expect(ship.hitCell).toStrictEqual([[0, 3]]);
 
-//   });
+  });
 
-//   test('should say Ship sunk', () => {
-//     const ship = new Ship(1, [[0, 3]]);
-//     const selectedCell = [0, 3]
+  test('should say Ship sunk', () => {
+    const ship = new Ship(1, [[0, 3]]);
+    const selectedCell = [0, 3]
 
-//     ship.hit(selectedCell);
-//     expect(ship.sunked).toBe(true);
+    ship.hit(selectedCell);
+    expect(ship.sunked).toBe(true);
 
-//   });
+  });
 
-// });
+});
 
-// describe('Ship occupiedCells', () => {
-//   test('Horizontal Positive', () => {
-//     const ship = new Ship(5, [[0, 0], [0, 4]]);
-//     expect(ship.occupiedCells).toStrictEqual([[0, 0], [0, 1], [0, 2], [0, 3], [0, 4]]);
-//   });
+describe('Ship occupiedCells', () => {
+  test('Horizontal Positive', () => {
+    const ship = new Ship(5, [[0, 0], [0, 4]]);
+    expect(ship.occupiedCells).toStrictEqual([[0, 0], [0, 1], [0, 2], [0, 3], [0, 4]]);
+  });
 
-//   test('Horizontal Negative', () => {
-//     const ship = new Ship(5, [[0, 4], [0, 0]]);
-//     expect(ship.occupiedCells).toStrictEqual([[0, 4], [0, 3], [0, 2], [0, 1], [0, 0]]);
-//   });
+  test('Horizontal Negative', () => {
+    const ship = new Ship(5, [[0, 4], [0, 0]]);
+    expect(ship.occupiedCells).toStrictEqual([[0, 4], [0, 3], [0, 2], [0, 1], [0, 0]]);
+  });
 
-//   test('Vertical Positive', () => {
-//     const ship = new Ship(5, [[0, 0], [4, 0]]);
-//     expect(ship.occupiedCells).toStrictEqual([[0, 0], [1, 0], [2, 0], [3, 0], [4, 0]]);
-//   });
-//   test('Vertical Negative', () => {
-//     const ship = new Ship(5, [[4, 0], [0, 0]]);
-//     expect(ship.occupiedCells).toStrictEqual([[4, 0], [3, 0], [2, 0], [1, 0], [0, 0]]);
-//   });
+  test('Vertical Positive', () => {
+    const ship = new Ship(5, [[0, 0], [4, 0]]);
+    expect(ship.occupiedCells).toStrictEqual([[0, 0], [1, 0], [2, 0], [3, 0], [4, 0]]);
+  });
+  test('Vertical Negative', () => {
+    const ship = new Ship(5, [[4, 0], [0, 0]]);
+    expect(ship.occupiedCells).toStrictEqual([[4, 0], [3, 0], [2, 0], [1, 0], [0, 0]]);
+  });
 
-//   test('Single cell Ship', () => {
-//     const ship = new Ship(1, [[0, 0]]);
-//     expect(ship.occupiedCells).toStrictEqual([[0, 0]]);
-//   });
+  test('Single cell Ship', () => {
+    const ship = new Ship(1, [[0, 0]]);
+    expect(ship.occupiedCells).toStrictEqual([[0, 0]]);
+  });
 
-// });
+});
 
 describe("GameBoard tests", () => {
 
   test("Board Size", () => {
     const myBoard = new GameBoard(0, 9);
     myBoard.placeShip(3, [[0, 0], [0, 2]]);
-    console.log(myBoard.size)
     expect(myBoard.size).toStrictEqual([[0, 0], [9, 9]])
   });
 
@@ -87,4 +86,69 @@ describe("GameBoard tests", () => {
     myBoard.receiveAttack([0, 3]);
     expect(myBoard.missedShot).toStrictEqual([[0, 3]])
   });
+
+  test("Attack on the same Cell raise Error", () => {
+    const myBoard = new GameBoard(0, 9);
+    myBoard.placeShip(3, [[0, 0], [0, 2]]);
+    myBoard.receiveAttack([0, 3]);
+    myBoard.receiveAttack([0, 3]);
+    expect(myBoard.missedShot).toStrictEqual([[0, 3]])
+  });
+})
+
+describe("Create Player , and simulate Game", () => {
+  let player1;
+  let player2;
+
+  beforeEach(() => {
+    // Fresh instances for each test
+    player1 = new Player("Sewsi", true);
+    player2 = new Player("computer", false);
+
+    player1.board.placeShip(3, [[0, 0], [0, 2]]);
+    player2.board.placeShip(3, [[0, 0], [0, 2]]);
+  });
+
+  test("Create a computer player", () => {
+    expect(player2.name).toBe("computer");
+    expect(player2.isReal).toBe(false);
+    expect(player2.board).toBeInstanceOf(GameBoard);
+  });
+
+  test("Create a user player", () => {
+    expect(player1.name).toBe("Sewsi");
+    expect(player1.isReal).toBe(true);
+    expect(player1.board).toBeInstanceOf(GameBoard);
+  });
+
+  test("Simulate a computer attack", () => {
+    player2.attack(player1.board);
+    // Check that computer made an attack (either hit or miss)
+    const totalAttacks = player1.board.ships[0].hitCell.length + player1.board.missedShot.length;
+    expect(totalAttacks).toBe(1);
+  });
+
+  test("Computer doesn't attack same cell twice", () => {
+    for (let i = 0; i < 10; i++) {
+        player2.attack(player1.board);
+    }
+    const totalAttacks = player1.board.ships[0].hitCell.length + 
+                         player1.board.missedShot.length;
+    expect(totalAttacks).toBe(10);
+    expect(player2.availableCell.length).toBe(100 - 10); 
+});
+  
+  test("Simulate a user attack", () => {
+    player1.attack(player2.board, [0, 0]);
+    // Check that the ship was hit
+    expect(player2.board.ships[0].hitCell).toContainEqual([0, 0]);
+  });
+
+  test("Simulate a user sinking a ship", () => {
+    player1.attack(player2.board, [0, 0]);
+    player1.attack(player2.board, [0, 1]);
+    player1.attack(player2.board, [0, 2]);
+    expect(player2.board.ships[0].sunked).toBe(true);
+  });
+
 })
